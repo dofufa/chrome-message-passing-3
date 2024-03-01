57507b7875c18dcce44326a989d2faf0a46b02a6
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-03-01 00:43:16 -0800

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,15 +1,17 @@
 // popup.js
-// job  : Sends message to service worker, gets response
+// job  : Sends message to content script, gets response
 // lic  : https://opensource.org/license/mit
-// repo : https://github.com/dofufa/chrome-message-passing-1
+// repo : https://github.com/dofufa/chrome-message-passing-3
 console.log('ext-popup');
 
 const message = { name: 'from-popup',
   options: { ramen: ['string', 'string', 'string'] }
 };
 
-chrome.runtime.sendMessage(message, (response) => {
-  console.log('response received by popup, from service worker')
-  console.log('response: %o', response);
-  console.log();
+chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+  chrome.tabs.sendMessage(tabs[0].id, {"message": message},{}, (response) => {
+    console.log('response received by popup, from content script')
+    console.log('response: %o', response);
+    console.log();
+  });
 });
